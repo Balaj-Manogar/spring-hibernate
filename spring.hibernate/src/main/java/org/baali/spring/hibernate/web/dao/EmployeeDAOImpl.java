c2704594f2269fdae6ed0d9638c472c4f14fa087
Balaji. M <Balaji@192.168.0.101>
2015-05-28 07:48:55 +0530

    Service class implementation

--- spring.hibernate/src/main/java/org/baali/spring/hibernate/web/dao/EmployeeDAOImpl.java
+++ spring.hibernate/src/main/java/org/baali/spring/hibernate/web/dao/EmployeeDAOImpl.java
@@ -8,7 +8,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class EmployeeDAOImpl implements IEmployeeDAO
 {
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeDAOImpl.class);
---
+++ spring.hibernate/src/main/java/org/baali/spring/hibernate/web/service/PersonServiceImpl.java
@@ -0,0 +1,54 @@
+package org.baali.spring.hibernate.web.service;
+
+import java.util.List;
+
+import org.baali.spring.hibernate.model.Employees;
+import org.baali.spring.hibernate.web.dao.IEmployeeDAO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PersonServiceImpl implements IPersonService
+{
+	private IEmployeeDAO employeeDAO;
+	
+	
+	public void setEmployeeDAO(IEmployeeDAO employeeDAO)
+	{
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Transactional
+	public void addEmployee(Employees employee)
+	{
+		this.employeeDAO.addEmployee(employee);
+	}
+
+	@Transactional
+	public void updateEmployee(Employees employee)
+	{
+		this.employeeDAO.updateEmployee(employee);
+
+	}
+
+	@Transactional
+	public List<Employees> employeesList()
+	{
+		// TODO Auto-generated method stub
+		return this.employeeDAO.employeesList();
+	}
+
+	@Transactional
+	public Employees getEmployeeByNo(int empNo)
+	{
+		// TODO Auto-generated method stub
+		return this.employeeDAO.getEmployeeByNo(empNo);
+	}
+
+	@Transactional
+	public void removeEmployee(int empNo)
+	{
+		this.employeeDAO.removeEmployee(empNo);
+	}
+
+}
